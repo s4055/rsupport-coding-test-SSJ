e927d23eb7e831ef720a92058c28c5f03aaa0009
s4055 <sijin901@gmail.com>
2025-03-12 00:45:40 +0900

    feat: init project
    feat: init project

---
+++ src/main/java/com/rsupport/notice/management/NoticeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.rsupport.notice.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticeManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rsupport/notice/management/NoticeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsupport.notice.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
