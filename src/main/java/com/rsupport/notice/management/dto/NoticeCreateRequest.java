0211e7a7c6feccf41fed48d46cc6aeb76e136597
s4055 <sijin901@gmail.com>
2025-03-12 23:27:40 +0900

    feat: 공지사항 등록 유효성 검사 추가
    feat: 공지사항 등록 유효성 검사 추가

--- src/main/java/com/rsupport/notice/management/dto/NoticeCreateRequest.java
+++ src/main/java/com/rsupport/notice/management/dto/NoticeCreateRequest.java
@@ -1,15 +1,38 @@
 package com.rsupport.notice.management.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
+import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
 public class NoticeCreateRequest {
-  @NotBlank private String author;
-  @NotBlank private String title;
-  @NotBlank private String content;
-  @NotNull private LocalDateTime startDate;
-  @NotNull private LocalDateTime endDate;
+  @NotBlank(message = "작성자는 필수 입니다.")
+  private String author;
+
+  @NotBlank(message = "제목은 필수 입니다.")
+  private String title;
+
+  @NotBlank(message = "내용은 필수 입니다.")
+  private String content;
+
+  @NotNull(message = "공지 시작일시는 필수 입니다.")
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+  private LocalDateTime startDate;
+
+  @NotNull(message = "공지 종료일시는 필수 입니다.")
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+  private LocalDateTime endDate;
+
+  @AssertTrue(message = "공지 시작일시는 현재 일시보다 이전일 수 없습니다.")
+  public boolean isStartDateValid() {
+    return startDate.isAfter(LocalDateTime.now());
+  }
+
+  @AssertTrue(message = "공지 종료일시는 공지 시작일보다 이전일 수 없습니다.")
+  public boolean isEndDateValid() {
+    return endDate.isAfter(startDate);
+  }
 }
