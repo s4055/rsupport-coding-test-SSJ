580ec0a7baeb66e314ac95d95cf796599b7b4bbe
s4055 <sijin901@gmail.com>
2025-03-17 01:09:25 +0900

    feat: Swagger 설정 수정
    feat: Swagger 설정 수정

--- src/main/java/com/rsupport/notice/management/config/SwaggerConfig.java
+++ src/main/java/com/rsupport/notice/management/config/SwaggerConfig.java
@@ -2,11 +2,21 @@ package com.rsupport.notice.management.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 @Configuration
 public class SwaggerConfig {
+
+  /**
+   * Swagger 정보 설정
+   *
+   * @return the open api
+   */
   @Bean
   public OpenAPI openAPI() {
     return new OpenAPI()
@@ -15,5 +25,16 @@ public class SwaggerConfig {
                 .title("공지사항 관리 API")
                 .description("공지사항 등록, 조회, 수정, 삭제 API 문서")
                 .version("1.0"));
+  }
+
+  /**
+   * Swagger 통한 DTO + MultipartFile 요청에 대한 예외 처리
+   *
+   * @param converter the converter
+   */
+  public SwaggerConfig(MappingJackson2HttpMessageConverter converter) {
+    List<MediaType> supportMediaTypes = new ArrayList<>(converter.getSupportedMediaTypes());
+    supportMediaTypes.add(new MediaType("application", "octet-stream"));
+    converter.setSupportedMediaTypes(supportMediaTypes);
   }
 }
